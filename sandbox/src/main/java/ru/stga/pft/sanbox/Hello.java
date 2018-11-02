098560a66278e868a48aaa05211a40e5edc533d2
Eugen <korkers@bk.ru>
2018-11-02 14:37:56 +0300

    счет

--- sandbox/src/main/java/ru/stga/pft/sanbox/Hello.java
+++ sandbox/src/main/java/ru/stga/pft/sanbox/Hello.java
@@ -1,6 +1,18 @@
 package ru.stga.pft.sanbox;
 
+
+
 public class Hello {
-public static void main(String[] args) { System.out.println("ru.stga.pft.sanbox.Hello, World!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println(2 + 2);
+        System.out.println(1 /2); // 0
+        System.out.println(1.0 /2); //0.5
+
+        int l = 6;
+        int s= l*l;
+        System.out.println("s kw" + l + "=" +s);
+
+    }
 }
